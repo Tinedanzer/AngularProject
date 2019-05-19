ca9d1be0e6af6e7efa1c3d33a1dfab0144ea1129
Tinedanzer <johnhillallen@gmail.com>
2019-05-19 18:01:55 -0500

    added a new component and registered it w/ the module

--- Tour-of-Carebears/src/app/app.module.ts
+++ Tour-of-Carebears/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NameComponent } from './name-labels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ Tour-of-Carebears/src/app/name-labels.component.ts
@@ -0,0 +1,13 @@
+import{Component} from '@angular/core';
+
+@Component({
+    // selector:'app-root',
+    // templateUrl:'./app.component.html',
+    // styleUrls: ['./app.component.css']
+    selector: 'names',
+    template: '<h2>Names of Carebears:</h2>',
+
+})
+export class NameComponent{
+    name_labels = 'pew pew';
+}
