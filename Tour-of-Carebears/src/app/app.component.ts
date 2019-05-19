088b788d8fe2975814331e1f7a2dc02a25350a6a
Tinedanzer <johnhillallen@gmail.com>
2019-05-19 18:10:09 -0500

    added new component/registered module and some DOM manipulation

--- Tour-of-Carebears/src/app/app.component.ts
+++ Tour-of-Carebears/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour-of-Carebears';
+  name_labels= 'test test test PEW';
 }
--- Tour-of-Carebears/src/app/name-labels.component.ts
+++ Tour-of-Carebears/src/app/name-labels.component.ts
@@ -1,13 +1,10 @@
 import{Component} from '@angular/core';
 
 @Component({
-    // selector:'app-root',
-    // templateUrl:'./app.component.html',
-    // styleUrls: ['./app.component.css']
     selector: 'names',
     template: '<h2>Names of Carebears:</h2>',
 
 })
 export class NameComponent{
-    name_labels = 'pew pew';
+    
 }
