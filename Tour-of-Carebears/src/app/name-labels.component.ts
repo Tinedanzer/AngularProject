71ad62585aff27aaa6399dc7baf67c9384d57bcb
Tinedanzer <johnhillallen@gmail.com>
2019-05-20 01:07:17 -0500

    more learning

--- Tour-of-Carebears/src/app/name-labels.component.ts
+++ Tour-of-Carebears/src/app/name-labels.component.ts
@@ -2,9 +2,19 @@ import{Component} from '@angular/core';
 
 @Component({
     selector: 'names',
-    template: '<h2>Names of Carebears:</h2>',
+    template: `<h2>{{"Names of Carebears: " + getList() }}</h2>
+            <ul *ngFor='let x of carebears'>
+                {{ x }}
+            </ul>`
 
 })
 export class NameComponent{
-    
+    // objBear={
+    //     1:'aroo'
+    // }
+    title=['Grumpy Bear', 'aroo', 'pew pew'].join('™ ▬ ');
+    carebears=['Grumpy Bear', 'sunshine bear']
+    getList(){
+        return this.title;
+    }
 }
