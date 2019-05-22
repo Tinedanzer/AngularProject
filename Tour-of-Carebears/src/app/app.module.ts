f1658ff11137dead3c7d706b86b8ab49e62b1d82
Tinedanzer <johnhillallen@gmail.com>
2019-05-21 21:03:30 -0500

    adding a two-way binding

--- Tour-of-Carebears/src/app/app.module.ts
+++ Tour-of-Carebears/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { NameComponent } from './name-labels.component';
 import { NamesService } from './name-labels.service';
@@ -13,7 +13,8 @@ import { CarebearsComponent } from './carebears/carebears.component';
     CarebearsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     NamesService
